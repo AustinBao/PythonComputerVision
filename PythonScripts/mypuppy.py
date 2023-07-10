67d4c5e19048b9196629a12348938a0f8be511f6
Austin Bao <austin_dun@outlook.com>
2023-07-09 22:52:35 -0600

    figuring git out

--- PythonScripts/mypuppy.py
+++ PythonScripts/mypuppy.py
@@ -6,7 +6,7 @@ img = cv2.imread('Computer-Vision-with-Python/DATA/00-puppy.jpg')
 while True: 
     cv2.imshow('Puppy', img)
     
-    # if we waited at least 1 millisecond AND we have pressed the esc key
+    # if we waited at least 1 millisecond AND we have pressed the esc key.
     if cv2.waitKey(1) & 0xFF == 27: 
         #can also have 0xFF == ord('*insert key from keybaord') to specify the key to close the window.
         break
