80be43873ca04069f8f8ed16110f7784d070c5f9
Austin Bao <austin_dun@outlook.com>
2023-07-11 19:17:32 -0600

    Start of Section 4

--- PythonScripts/AssessmentSec3.py
+++ PythonScripts/AssessmentSec3.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-
+# Functions
 def draw_circle(event,x,y,flags,param):
     if event == cv2.EVENT_RBUTTONDOWN:
         cv2.circle(img,(x,y),100,(0,0,255), 10)
 
         
-        
+# Create IMG
 img = cv2.imread("../DATA/dog_backpack.jpg")
 
 cv2.namedWindow(winname='dog')
@@ -15,7 +15,7 @@ cv2.namedWindow(winname='dog')
 cv2.setMouseCallback('dog',draw_circle)
 
 
-
+# Stay Open
 while True: 
     cv2.imshow('dog',img)
 
